bccee784e50da8b1d8b80ee91544eb929529b492
Sara Alqallaf <202107125@stu.uob.edu.bh>
2025-08-10 16:52:11 +0300

    create the books state

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,20 @@
 // src/App.jsx
+import { useState } from 'react';
+import './App.css';
+import Bookshelf from './Bookshelf.jsx';
 
 const App = () => {
-  return <h1>Hello world!</h1>;
+  const [books , setbooks] = useState([{
+    title: "",
+    author: ""
+  }])
+
+  return (
+    <>
+      <h1>My Bookshelf</h1>
+      <Bookshelf />
+    </>
+  );
 };
 
 export default App;
